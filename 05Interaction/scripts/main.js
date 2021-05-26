b8e24798bc8db41541518202a73cc35e968c41c6
ZeynabAbdulhanan <61313171+ZeynabAbdulhanan@users.noreply.github.com>
2021-05-26 14:19:22 +0200

    Update main.js

--- 05Interaction/scripts/main.js
+++ 05Interaction/scripts/main.js
@@ -12,7 +12,7 @@
 //setInterval(spin, 16);
 
 
-// myOtherBox.addEventListener('mouseenter', function(){
-// 	rotationSpeed = 0.01;
-// 	console.log('mouse enter');
-// });
+myOtherBox.addEventListener('mouseenter', function(){
+ 	rotationSpeed = 0.01;
+	console.log('mouse enter');
+});
